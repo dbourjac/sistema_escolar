0ef72b0d250b1b2db82bbc737c96bfea44a073f0
dbourjac <110423970+dbourjac@users.noreply.github.com>
2024-04-18 22:10:55 -0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,36 +1,50 @@
-<?php
-// Pagina principal del portal
-include_once("inc/header.php");
-include("inc/sesiones.php");
-include("inc/consultas.php");
-validarSesion();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio de Sesión Exitoso</title>
+    <title>Portal de alumnos</title>
     <link rel="stylesheet" href="css/menu.css">
 </head>
 <body>
-    <nav>
-        <?php menu2(); ?>
-    </nav>
-    <div class="content">
-        <h1>Bienvenido al Sistema Escolar</h1>
-        <p>¡Hola, <?php echo $_SESSION["nombreUsuario"]; ?>! Has iniciado sesión exitosamente en el sistema escolar. ¡Bienvenido de vuelta!</p>
-        <?php
-        encabezado();
-        menu();
-        $carrera = mostrarCarrera();
-        if($carrera) {
-            echo "<p>Estás inscrito en la carrera de: " . $carrera . "</p>";
-        } else {
-            echo "<p>Aún no estás inscrito en ninguna carrera.</p>";
-        }
-        ?>
-        <a href="logout.php">Cerrar Sesión</a>
+<div class="container">
+    <div class="left-section">
+        <div class="content">
+            <div class="content-details">
+                <?php
+                include_once("inc/header.php");
+                include("inc/sesiones.php");
+                include("inc/consultas.php");
+                validarSesion();
+
+                encabezado();
+                $carrera = mostrarCarrera();
+                if ($carrera) {
+                    echo  $carrera;
+                } else {
+                    echo "<p>Aún no estás inscrito en ninguna carrera.</p>";
+                }
+                menu();
+                ?>
+            </div>
+            <div class="profile-image-container">
+                <img class="profile-image" src="img/dilan.jpg" alt="Silueta de usuario">
+            </div>
+        </div>
     </div>
+</div>
+<div class="enmedio">
+    <p>¡Hola, <?php echo htmlspecialchars($_SESSION["nombreUsuario"]); ?>! Has iniciado sesión exitosamente en el sistema escolar. ¡Bienvenido de vuelta!</p>
+</div>
+<div class="top-section">
+    <div class="header">
+        <div class="portal-caja">
+            <h1>Portal de alumnos</h1>
+        </div>
+        <nav>
+            <?php menu2(); ?>
+        </nav>
+    </div>
+</div>
 </body>
 </html>
